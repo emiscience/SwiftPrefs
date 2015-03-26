d8b7ccdde4a4ffd369475d71e09a96841d924526
Karl Moskowski <kmoskowski@me.com>
2015-03-26 14:58:03 -0400

    Account for the toolbar height; fix the constraints within the tabViews

--- SwiftPrefs/PreferencesViewController.swift
+++ SwiftPrefs/PreferencesViewController.swift
@@ -3,7 +3,7 @@ import Cocoa
 // in the storyboard, ensure the view controller's transition checkboxes are all off, and the NSTabView's delegate is set to this controller object
 
 class PreferencesViewController: NSTabViewController {
-   
+    
     lazy var originalSizes = [String : NSSize]()
     
     // MARK: - NSTabViewDelegate
@@ -18,28 +18,27 @@ class PreferencesViewController: NSTabViewController {
         tabViewItem.view!.hidden = true
         
         // for each tabViewItem, save the original, as-laid-out-in-IB view size, so it can be used to resize the window with the selected tab changes
-        var size = (tabViewItem.view?.frame.size)!
         var originalSize = self.originalSizes[tabViewItem.label]
-        if (originalSize != nil) {
-            size = originalSize!
-        } else {
-            self.originalSizes[tabViewItem.label] = size
-        }
-        
-        let window = self.view.window
-        if (window != nil) {
-            window?.title = tabViewItem.label
-
-            var frame = (window?.frame)!
-            frame.origin.y = frame.origin.y + (frame.size.height - size.height)
-            frame.size.height = size.height;
-            frame.size.width = size.width;
-            window?.setFrame(frame, display: true, animate: true)
+        if (originalSize == nil) {
+            self.originalSizes[tabViewItem.label] = (tabViewItem.view?.frame.size)!
         }
     }
     
     override func tabView(tabView: NSTabView, didSelectTabViewItem tabViewItem: NSTabViewItem) {
         super.tabView(tabView, didSelectTabViewItem: tabViewItem)
+        
+        let window = self.view.window
+        if (window != nil) {
+            window?.title = tabViewItem.label
+            let size = (self.originalSizes[tabViewItem.label])!
+            var contentFrame = (window?.frameRectForContentRect(NSMakeRect(0.0, 0.0, size.width, size.height)))!
+            var frame = (window?.frame)!
+            frame.origin.y = frame.origin.y + (frame.size.height - contentFrame.size.height)
+            frame.size.height = contentFrame.size.height;
+            frame.size.width = contentFrame.size.width;
+            window?.setFrame(frame, display: true, animate: true)
+        }
+        
         tabViewItem.view?.hidden = false
     }
 }
