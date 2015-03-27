c6838ddab8edcb284bd72650363f3192cb3928f7
Karl Moskowski <kmoskowski@me.com>
2015-03-27 08:43:26 -0400

    Fix the brief flash of the window's new size before it's animated

---
+++ SwiftPrefs/GeneralPreferencesViewController.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+class GeneralPreferencesViewController: NSViewController {
+    
+    // disable the layout constraint on the right side of the button, so the window doesn't briefly assume its final size before it's animated
+    
+    @IBOutlet weak var constraint : NSLayoutConstraint!
+    var oldPriority : NSLayoutPriority!
+    
+    override func viewWillAppear() {
+        self.oldPriority = self.constraint.priority
+    }
+
+    override func viewDidDisappear() {
+        self.constraint.priority = 1
+    }
+    
+    override func viewDidAppear() {
+        self.constraint.priority = self.oldPriority
+    }
+    
+}
